bc86c651ea4366fab28c25e591906e562c5c875d
hmJeon <hmjeon@aiblab.co.kr>
2024-12-09 10:53:25 +0900

    리스트 페이지 배치 및 페이징 처리

--- src/components/shared/Spacing.tsx
+++ src/components/shared/Spacing.tsx
@@ -1,19 +1,24 @@
+import { colors, Colors } from '@/styles/colorPalette'
 import styled from '@emotion/styled'
 
 interface SpacingProps {
   size?: number
   direction?: 'vertical' | 'horizontal'
+  backgroundColor?: Colors
 }
 
 const Spacing = styled.div<SpacingProps>`
-  ${({ size = 16, direction = 'vertical' }) =>
+  ${({ size, direction = 'vertical' }) =>
     direction === 'vertical'
       ? `
-        height: ${size}px
-        `
+        height: ${size}px;
+      `
       : `
-        width: ${size}px
+        width: ${size}px;
       `}
+
+  ${({ backgroundColor }) =>
+    backgroundColor && `background-color: ${colors[backgroundColor]};`}
 `
 
 export default Spacing
