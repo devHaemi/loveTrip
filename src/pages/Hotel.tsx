8f9793cad1933c7ddb9ccf6e2d868567b3114e5a
hmJeon <hmjeon@aiblab.co.kr>
2024-12-15 14:24:58 +0900

    호텔 상세 컨텐츠 구현

--- src/pages/Hotel.tsx
+++ src/pages/Hotel.tsx
@@ -1,7 +1,7 @@
 import Carousel from '@/components/hotel/Carousel'
+import Contents from '@/components/hotel/Contents'
 import useHotel from '@/components/hotel/hooks/useHotel'
 import Top from '@/components/shared/Top'
-import { css } from '@emotion/react'
 import { useParams } from 'react-router-dom'
 
 function HotelPage() {
@@ -13,19 +13,15 @@ function HotelPage() {
     return <div>Loading...</div>
   }
 
-  const { name, comment, images } = data
+  const { name, comment, images, contents } = data
 
   return (
     <div>
       <Top title={name} subTitle={comment} />
       <Carousel images={images} />
+      <Contents contents={contents} />
     </div>
   )
 }
-
-const containerStyles = css`
-  padding: 0 24px;
-  height: 300px;
-`
 
 export default HotelPage
